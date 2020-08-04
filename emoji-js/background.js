24d3a8eb2a4f1c37ecd4dfaee754be157d8fb646
JF1HNL <kimossan88@gmail.com>
2020-08-04 13:39:52 +0900

    emoji-js いらない部分削除

--- emoji-js/background.js
+++ emoji-js/background.js
@@ -1,9 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.sendMessage(tab.id, "Action");
 });
-
-window.onload = function(){
-  var script=document.createElement('script');
-  script.src = "./node_modules/emoji-js/lib/emoji.js"
-  document.body.appendChild(script)
-}
