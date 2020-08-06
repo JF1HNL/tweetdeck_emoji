8d7e3917dbab23e3ecc2717b3b58ce92f73b7863
ilim <ilim0t14@gmail.com>
2020-08-07 04:03:11 +0900

    Bugfix

--- emojidex/replace.js
+++ emojidex/replace.js
@@ -1,6 +1,5 @@
-                  ":" // 通常のコロン
-const imitation = "ː" // IPA発音記号
-// この行以降のコードではミス防止のため、コロンのみ使う
+const ZWSP = "​" // ゼロ幅スペース U+200B
+const ZWSP2 = "‌" // ゼロ幅スペース U+2009
 
 
 document.addEventListener('keydown', event => {
@@ -14,15 +13,16 @@ document.addEventListener('keydown', event => {
     const { target } = event
 
     const candidates = new Set()
-    target.value = target.value.replace(/:([^: ]+?):/g, (match, p1) => {
+    target.value = target.value.replace(new RegExp(`:([^: ${ZWSP}${ZWSP2}]+?):`, 'g'), (match, p1) => {
       candidates.add(p1)
-      return imitation + p1 + imitation
+      return `:${ZWSP}${p1}${ZWSP2}:`
     })
 
-    for (candidate of candidates) {
+
+    candidates.forEach(candidate => {
       chrome.runtime.sendMessage({ code: candidate }, emoji => {
-        target.value = target.value.replace(new RegExp(imitation + candidate + imitation, 'g'), emoji || `:${candidate}:`)
+        target.value = target.value.replace(new RegExp(`:${ZWSP}${candidate}${ZWSP2}:`, 'g'), emoji || `:${candidate}:`)
       })
-    }
+    })
   })
 })
