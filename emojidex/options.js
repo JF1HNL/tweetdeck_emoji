cef5bf0021bc2cfc219045797d3830b5af49f24d
JF1HNL <kimossan88@gmail.com>
2020-08-20 18:11:08 +0900

    お試し

--- emojidex/options.js
+++ emojidex/options.js
@@ -1,5 +1,10 @@
-chrome.extension.getBackgroundPage().console.log(document.querySelector('#push'));
+chrome.extension.getBackgroundPage().console.log(document.querySelector('#emojiStore'));
 localStorage.setItem('test', 0)
 document.querySelector('#inc').addEventListener("click", () => localStorage.setItem('test', 1))
 document.querySelector('#dec').addEventListener("click", () => localStorage.setItem('test', -1))
-document.querySelector('#emoji').value = localStorage.getItem('tweetdeck_emoji-emojiStore')
+const emojiStore =  localStorage.getItem('tweetdeck_emoji-emojiStore')
+document.querySelector('#emojiStore').value = emojiStore ? emojiStore.replace("{", "{\n").replace("}", "\n}").replace(/,/g, ",\n") : ""
+document.querySelector('#emojiRegister').addEventListener("click", () => {
+  const str = document.querySelector('#emojiStore').value
+  chrome.extension.getBackgroundPage().console.log(JSON.parse(str));
+})
