ee9f58ae3c01d28f22e0fe66db2de2bd14d20fe6
JF1HNL <kimossan88@gmail.com>
2021-05-03 18:18:14 +0900

    update : 1.4.0 : 絵文字の検索結果に合わせて出力を変更する

--- emojidex/background.js
+++ emojidex/background.js
@@ -2,25 +2,25 @@ const mapToObject = map =>
 	[...map].reduce((l,[k,v]) => Object.assign(l, {[k]:v}), {})
 const objectToMap = object =>
 	new Map(Object.entries(object))
-	
+const nullToStr = (emoji) => emoji === null ? "🙅‍♂️" : emoji
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	const localStorageData = localStorage.getItem('tweetdeck_emoji-emojiStore')
 	const emojiStore = localStorageData ? objectToMap(JSON.parse(localStorageData)) : new Map()
 	const { code } = message
 	if (emojiStore.has(code)) {
-		sendResponse(emojiStore.get(code))
+		sendResponse(nullToStr(emojiStore.get(code)))
 		return true
 	}
 	fetch('https://www.emojidex.com/api/v1/emoji/' + code)
 		.then(async response => {
 			if (response.ok) {
 				const data = await response.json()
-				sendResponse(data.moji)
+				sendResponse(nullToStr(data.moji))
 				emojiStore.set(code, data.moji)
 				localStorage.setItem('tweetdeck_emoji-emojiStore',　JSON.stringify(mapToObject(emojiStore)))
 				return
 			}
-			sendResponse(false)
+			sendResponse("🙅‍♀️")
 		})
 
 	return true
